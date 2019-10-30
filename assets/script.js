57ae82ef134737642c9d3e9ae337c3823c2047bb
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-10-30 09:25:54 +0000

    added new script tag for places and maps api

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,4 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
-
 function activateTravellingFrom () {
     var input = document.getElementById('travelling_from');
-    var autocomplete = new.google.maps.Autocomplete(input);
+    var autocomplete = new google.maps.Autocomplete(input);
 }
