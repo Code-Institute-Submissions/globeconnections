838c12dd0c84075ea00d017160ad82d6d21bf63d
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-10-31 10:58:22 +0000

    added new javascript

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,12 @@
 function initPlacesSearch(){
     var input = document.getElementById('travelling_from');
+    var input2 = document.getElementById('travelling_to');
+    var input3 = document.getElementById('restaurant_search');
+    var input4 = document.getElementById('attraction_search');
     var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-function initPlacesSearch(){
-    var input = document.getElementById('travelling_to');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-function initPlacesSearch(){
-    var input = document.getElementById('restaurant_search');
-    var autocomplete = new google.maps.places.Autocomplete(input);
-}
-
-function initPlacesSearch(){
-    var input = document.getElementById('attraction_search');
-    var autocomplete = new google.maps.places.Autocomplete(input);
+    var autocomplete2 = new google.maps.places.Autocomplete(input2);
+    var autocomplete3 = new google.maps.places.Autocomplete(input3);
+    var autocomplete4 = new google.maps.places.Autocomplete(input4);
 }
 
 jQuery(function($) {
@@ -25,3 +16,4 @@ jQuery(function($) {
 jQuery(function($) {
         $("#datepicker2").datetimepicker();
     });
+
