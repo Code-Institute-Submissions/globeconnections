483557af7b1ddb1164a3f86028eef7d9103ecd7c
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-10-31 09:57:19 +0000

    added the date time picker and the autocomplete places API to the top 4 fields in the search form

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,15 @@ function initPlacesSearch(){
     var autocomplete = new google.maps.places.Autocomplete(input);
 }
 
+function initPlacesSearch(){
+    var input = document.getElementById('travelling_to');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
 jQuery(function($) {
         $("#datepicker").datetimepicker();
+    });
+
+jQuery(function($) {
+        $("#datepicker2").datetimepicker();
     });
