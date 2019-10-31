8361f2b0d59152b68bf46c8ca19541f8920f0c6b
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-10-31 08:22:50 +0000

    added new script.js

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function initPlacesSearch(){
+    var input = document.getElementById('travelling_from');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+}
+
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
