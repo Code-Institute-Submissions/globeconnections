8f7099d824d107d6d8215c116c75d9926442a671
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-11-02 15:55:03 +0000

    added new javascript for the id map on line 44 of indexedDB.html

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,4 @@
 function initPlacesSearch(){
-    var input = document.getElementById('travelling_from');
-    var input2 = document.getElementById('travelling_to');
-    var input3 = document.getElementById('restaurant_search');
-    var input4 = document.getElementById('attraction_search');
+    var input = document.getElementById('map');
     var autocomplete = new google.maps.places.Autocomplete(input);
-    var autocomplete2 = new google.maps.places.Autocomplete(input2);
-    var autocomplete3 = new google.maps.places.Autocomplete(input3);
-    var autocomplete4 = new google.maps.places.Autocomplete(input4);
 }
-
-jQuery(function($) {
-        $("#datepicker").datetimepicker();
-    });
-
-jQuery(function($) {
-        $("#datepicker2").datetimepicker();
-    });
-
