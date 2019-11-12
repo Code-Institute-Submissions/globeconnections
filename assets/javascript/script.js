2f1c23d1a3dfd4cbde334c47e1faea9e4a60f3e2
TejalSaldanha <tejalsaldanhaone@outlook.com>
2019-11-12 16:29:08 +0000

    added js for results list display on the map

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -42,6 +42,43 @@ var hostnameRegexp = new RegExp('^https?://.+?/');
           });
           markers = [];
 
+});
+      }
+/* what i added */
+        function addResult(result, i) {
+        var results = document.getElementById('results');
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+
+        var tr = document.createElement('tr');
+        tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
+        tr.onclick = function() {
+          google.maps.event.trigger(markers[i], 'click');
+        };
+
+        var iconTd = document.createElement('td');
+        var nameTd = document.createElement('td');
+        var icon = document.createElement('img');
+        icon.src = markerIcon;
+        icon.setAttribute('class', 'placeIcon');
+        icon.setAttribute('className', 'placeIcon');
+        var name = document.createTextNode(result.name);
+        iconTd.appendChild(icon);
+        nameTd.appendChild(name);
+        tr.appendChild(iconTd);
+        tr.appendChild(nameTd);
+        results.appendChild(tr);
+      }
+
+      function clearResults() {
+        var results = document.getElementById('results');
+        while (results.childNodes[0]) {
+          results.removeChild(results.childNodes[0]);
+        }
+      }
+
+/* what i added */
+
           // For each place, get the icon, name and location.
           var bounds = new google.maps.LatLngBounds();
           var count = 0;
